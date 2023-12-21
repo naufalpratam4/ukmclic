1cf55a4d2f42fd0004008d8df85d299b41c30cda
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-12-21 23:07:14 +0700

    add about in landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import NavBar from "../components/section/Landing page/NavBar";
 import HeroSection from "../components/section/Landing page/hero";
+import About from "../components/section/about/about";
 
 const LandingPage = () => {
   return (
     <>
       <NavBar />
       <HeroSection />
+      <About />
     </>
   );
 };
