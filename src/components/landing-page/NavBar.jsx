fae113c365f70a3bdc17bae97a6fcb3955de5ea4
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-12-28 23:15:08 +0700

    add icon in public

--- src/components/landing-page/NavBar.jsx
+++ src/components/landing-page/NavBar.jsx
@@ -1,6 +1,6 @@
 import { HamburgerIcon } from "@chakra-ui/icons";
 import React from "react";
-// import { logoClic } from "../../../public/logoClic.png";
+import logoClic from "../../../public/icon/logoClic.png";
 function NavBar() {
   return (
     <div className="grid grid-cols-3 p-8 md:flex">
@@ -10,7 +10,7 @@ function NavBar() {
         </div>
       </div>
       <div className="md:order-1">
-        <img src="public/logoClic.png" alt="" />
+        <img src={logoClic} alt="" />
       </div>
     </div>
   );
