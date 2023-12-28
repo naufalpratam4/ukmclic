a84ea1c8633f0d5df589ecc1e8702d199545bd71
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-12-28 20:18:41 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/divisi" element="cek" />
+        tes
       </Routes>
     </BrowserRouter>
   );
