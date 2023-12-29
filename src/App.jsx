08c345a747bd32cd7c44ac6ef8f9e03cd3812372
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-12-29 10:28:46 +0700

    add router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import LandingPage from "./pages/LandingPage";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <div>
-        <LandingPage />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
