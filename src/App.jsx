d89e273a99f8100423af685f18146f53e14bfcf6
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-12-29 14:40:29 +0700

    update lur

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import DaftarAnggota from "./pages/DaftarAnggota";
+import Dashboard from "./pages/Dashboard";
 import LandingPage from "./pages/LandingPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
@@ -6,6 +8,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/daftar-anggota" element={<DaftarAnggota />} />
         </Routes>
       </BrowserRouter>
     </>
