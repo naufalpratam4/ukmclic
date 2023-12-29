8bb2edeb41b9bf9016a28da0142998687b4c2ce6
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-12-29 09:43:52 +0700

    update navbar

--- src/components/landing-page/NavBar.jsx
+++ src/components/landing-page/NavBar.jsx
@@ -3,19 +3,117 @@ import React from "react";
 import logoClic from "../../../public/icon/logoClic.png";
 function NavBar() {
   return (
-    <div className="grid grid-cols-3 p-8 md:flex">
-      <div className="md:order-2">
-        <div className="md:hidden">
-          <HamburgerIcon boxSize={32} />
+    <>
+      <div className="flex flex-wrap place-items-center sticky top-0">
+        <section className="relative mx-auto">
+          {/* navbar */}
+          <nav className="flex justify-between bg-white text-black w-screen border-b-2 ">
+            <div className="px-5 xl:px-12 py-6 flex w-full items-center">
+              <a className="text-3xl font-bold font-heading" href="#">
+                {/* <img class="h-9" src="logo.png" alt="logo"> */}
+                <img src={logoClic} alt="" />
+              </a>
+              {/* Nav Links */}
+              <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
+                <li>
+                  <a className="hover:text-orange-500" href="#">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a className="hover:text-orange-500" href="#">
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a className="hover:text-orange-500" href="#">
+                    Visi Misi
+                  </a>
+                </li>
+                <li>
+                  <a className="hover:text-orange-500" href="#">
+                    Divisi
+                  </a>
+                </li>
+              </ul>
+              {/* Header Icons */}
+              <div className="hidden xl:flex items-center space-x-5 ">
+                {/* Sign In / Register      */}
+                <a className="flex items-center hover:text-gray-200" href="#">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 hover:text-orange-500"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+            {/* Responsive navbar */}
+            <a className="xl:hidden flex mr-6 items-center" href="#">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 hover:text-gray-200"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span className="flex absolute -mt-5 ml-4">
+                <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75" />
+                <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
+              </span>
+            </a>
+            <a className="navbar-burger self-center mr-12 xl:hidden" href="#">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 hover:text-gray-200"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </a>
+          </nav>
+        </section>
+      </div>
+      {/* Does this resource worth a follow? */}
+      <div className="absolute bottom-0 right-0 mb-4 mr-4 z-10">
+        <div>
+          <a
+            title="Follow me on twitter"
+            href="https://www.twitter.com/asad_codes"
+            target="_blank"
+            className="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12"
+          >
+            <img
+              className="object-cover object-center w-full h-full rounded-full"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/512px-WhatsApp.svg.png"
+            />
+          </a>
         </div>
       </div>
-      <div className="md:order-1 flex">
-        <img src={logoClic} alt="" />
-      </div>
-      <div className="md:order-3 sm:hidden">
-        <img src={logoClic} alt="" />
-      </div>
-    </div>
+    </>
   );
 }
 
