bdcc9d408e5795408cdd4779fbc3e2c8c09b7c75
naufalpratam4 <naufal.d.pratama@gmail.com>
2024-01-04 19:49:07 +0700

    update hero

--- src/components/landing-page/Hero.jsx
+++ src/components/landing-page/Hero.jsx
@@ -5,33 +5,39 @@ import heroImage from "../../../public/icon/Group 59.png";
 
 function Hero() {
   return (
-    <div className="md:flex">
-      <div className="w-full md:order-2">
-        <img src={heroImage} alt="" className="w-full" />
-      </div>
-
-      <div className="p-4 md:p-32 md:order-1">
-        <div className="text-2xl font-semibold">
-          Selamat Datang di Website UKM CLIC Universitas Negeri Semarang
+    <>
+      <div className="md:flex md:order-1">
+        <div className="md:order-2 w-full">
+          <img
+            src={heroImage}
+            alt=""
+            className="h-96  object-cover object-center"
+          />
         </div>
-        <div className="pt-3">
-          <div className="text-xl text-gray-500">Kepengurusan 2023/2024</div>
-          <div className="text-xl font-bold pb-2">Kabinet Amanah</div>
-          <div className="grid grid-cols-2 text-xl font-bold">
-            <div>
-              <Icon as={FaPeopleLine} />{" "}
-              <span className="text-gray-500">7 Divisi</span>
-            </div>
-            <div>
+
+        <div className="w-full my-auto">
+          <div className="text-2xl font-semibold px-9 ">
+            Selamat Datang di Website UKM CLIC Universitas Negeri Semarang
+          </div>
+          <div className="pt-3 px-9">
+            <div className="text-xl text-gray-500">Kepengurusan 2023/2024</div>
+            <div className="text-xl font-bold pb-2">Kabinet Amanah</div>
+            <div className="grid grid-cols-2 text-xl font-bold">
               <div>
-                <Icon as={FaPeopleGroup} />{" "}
-                <span className="text-gray-500">50 Fungsionaris</span>
+                <Icon as={FaPeopleLine} />{" "}
+                <span className="text-gray-500">7 Divisi</span>
+              </div>
+              <div>
+                <div>
+                  <Icon as={FaPeopleGroup} />{" "}
+                  <span className="text-gray-500">50 Fungsionaris</span>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
